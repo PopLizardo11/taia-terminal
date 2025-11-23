5a6949ca06d035bb9ef70ed273d1d802209dc103
Pop <poplizardo4321@gmail.com>
2025-11-23 09:44:51 +0800

    Transfer initial progress on remote

---
+++ src/lib/shellPrompt.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let input: any;
+    let name = $state("guest");
+
+    function setFocus() {
+        input.focus();
+    }
+
+    function resizeInput() {
+        input.style.width = (input.value.length + 1) + "ch"
+    }
+</script>
+
+<div class="font-mono font-bold anitialised" style="color:  var(--color-text-base)">
+    <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.taia.dev</span><span></span><span>:~$</span>
+    <br>
+    <span>&gt;</span> <input type="text" bind:this={input} onblur={setFocus} oninput={resizeInput} autofocus>
+</div>
+
+<style>
+    span {
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+    }
+
+    input {
+        width: 1ch;
+        max-width: 100%;
+        outline: none;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import ShellPrompt from "$lib/shellPrompt.svelte";
+
+    const themes = ["default", "hackerman", "githubdark", "synthwave", "dracula", "gruvboxdark"]
+</script>
+<div id="bg-container" class="h-screen w-screen flex items-center justify-center p-8" style="background-color: var(--color-bg-primary)">
+    <div id="term-interface" class="block w-full h-full border-2 rounded-xl box-border p-5 overflow-auto" style="border-color: var(--color-secondary)">
+        <ShellPrompt/>
+    </div>
+</div>
