575f20f9aa3b05ed26bda5d4c7d1299de52e45c6
Pop <poplizardo4321@gmail.com>
2025-11-28 18:51:06 +0800

    Added themes list in css and shellPrompt component

--- src/lib/shellPrompt.svelte
+++ src/lib/shellPrompt.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     let input: any;
-    let name = $state("guest");
+    let name: string = $state("guest");
 
     function setFocus() {
         input.focus();
@@ -9,10 +9,18 @@
     function resizeInput() {
         input.style.width = (input.value.length + 1) + "ch"
     }
+
+    let themes: string[] = ["default", "abyss", "amber", "arctic", "coffee", "cosmic", "cyberpunk", "forest", "synthwave", "toxic"]
+    let commands: string[] = ["help", "clear", "exit", "date", "banner", "coll", "theme", "contacts", "repo", "why"]
+
+    let terminalFeed: any[] = [
+        {type: "prompt", input: "banner"},
+        {type: "output", command: ["banner", ""]}
+    ]
 </script>
 
 <div class="font-mono font-bold anitialised" style="color:  var(--color-text-base)">
-    <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.taia.dev</span><span></span><span>:~$</span>
+    <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.aiaia.dev</span><span></span><span>:~$</span>
     <br>
     <span>&gt;</span> <input type="text" bind:this={input} onblur={setFocus} oninput={resizeInput} autofocus>
 </div>
