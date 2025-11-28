f9ae089fa65ec98e1c97e5fe8c5e445faad6bf46
Pop <poplizardo4321@gmail.com>
2025-11-28 22:55:22 +0800

    Fixed antialised spelling and changed aiaia back to taia

--- src/lib/shellPrompt.svelte
+++ src/lib/shellPrompt.svelte
@@ -12,15 +12,10 @@
 
     let themes: string[] = ["default", "abyss", "amber", "arctic", "coffee", "cosmic", "cyberpunk", "forest", "synthwave", "toxic"]
     let commands: string[] = ["help", "clear", "exit", "date", "banner", "coll", "theme", "contacts", "repo", "why"]
-
-    let terminalFeed: any[] = [
-        {type: "prompt", input: "banner"},
-        {type: "output", command: ["banner", ""]}
-    ]
 </script>
 
-<div class="font-mono font-bold anitialised" style="color:  var(--color-text-base)">
-    <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.aiaia.dev</span><span></span><span>:~$</span>
+<div class="font-mono font-bold antialised" style="color:  var(--color-text-base)">
+    <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.taia.dev</span><span></span><span>:~$</span>
     <br>
     <span>&gt;</span> <input type="text" bind:this={input} onblur={setFocus} oninput={resizeInput} autofocus>
 </div>
