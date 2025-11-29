709eac2f8161023fc9c7ce61c7d38fa8fe629994
Pop <poplizardo4321@gmail.com>
2025-11-29 16:55:23 +0800

    Added the terminal command system

--- src/lib/shellPrompt.svelte
+++ src/lib/shellPrompt.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
     let input: any;
     let name: string = $state("guest");
+    let { termBlocks = $bindable(), command = $bindable(), isDone = $bindable() } = $props();
 
     function setFocus() {
         input.focus();
     }
 
     function resizeInput() {
-        input.style.width = (input.value.length + 1) + "ch"
+        input.style.width = (input.value.length + 1) + "ch";
     }
 
-    let themes: string[] = ["default", "abyss", "amber", "arctic", "coffee", "cosmic", "cyberpunk", "forest", "synthwave", "toxic"]
-    let commands: string[] = ["help", "clear", "exit", "date", "banner", "coll", "theme", "contacts", "repo", "why"]
+    let themes: readonly string[] = ["default", "abyss", "amber", "arctic", "coffee", "cosmic", "cyberpunk", "forest", "synthwave", "toxic"];
+    let commands: readonly string[] = ["help", "clear", "exit", "date", "banner", "next", "prev", "all", "theme", "repo", "about", "refs", ];
+
+    function callCommand(curr: any) {
+        let text = curr.value.trim();
+        termBlocks.push({type: "response", command: text});
+        isDone = true
+        termBlocks.push({type: "prompt", command: "", isDone:false})
+    }
 </script>
 
 <div class="font-mono font-bold antialised" style="color:  var(--color-text-base)">
-    <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.taia.dev</span><span></span><span>:~$</span>
-    <br>
-    <span>&gt;</span> <input type="text" bind:this={input} onblur={setFocus} oninput={resizeInput} autofocus>
+    <form onsubmit={e => {
+        e.preventDefault();
+        callCommand(input);
+    }}>
+        <span style="color: var(--color-tertiary)">{name}</span><span>@</span><span style="color:var(--color-secondary)">term.taia.dev</span><span></span><span>:~$</span>
+        <br>
+        <span>&gt;</span> <input type="text" bind:this={input} oninput={resizeInput} onblur={setFocus} autofocus disabled={isDone} bind:value={command}>
+    </form>
 </div>
 
 <style>
@@ -28,7 +41,7 @@
     }
 
     input {
-        width: 1ch;
+        width: 7ch;
         max-width: 100%;
         outline: none;
     }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,20 @@
     import ShellPrompt from "$lib/shellPrompt.svelte";
     import ShellResponse from "$lib/shellResponse.svelte";
 
-    let termBlocks: any[] = [
-        {type: "prompt", input: "banner"},
-        {type: "shellRes", command: "banner"},
-        {type: "prompt", input: ""},
-        {type: "shellRes", command: "help"},
-    ]
+    let termBlocks: any[] = $state([
+        {type: "prompt", command: "banner", isDone:true},
+        {type: "response", command: "banner"},
+        {type: "prompt", command: "", isDone:false}
+    ])
 </script>
 <div id="bg-container" class="h-screen w-screen flex items-center justify-center p-8" style="background-color: var(--color-bg-primary)">
     <div id="term-interface" class="block w-full h-full border-2 rounded-xl box-border p-5 overflow-auto" style="border-color: var(--color-secondary)">
-        {#each termBlocks as block}
+        {#each termBlocks as block, i}
             {#if block.type === "prompt"}
-                <ShellPrompt/>
-            {:else if block.type === "shellRes"}
+                <ShellPrompt bind:termBlocks={termBlocks} command={block.command} isDone={block.isDone}/>
+            {:else if block.type === "response"}
+                <ShellResponse command={block.command}/>
+            {:else if block.type === "response"}
                 <ShellResponse command={block.command}/>
             {/if}
         {/each}
