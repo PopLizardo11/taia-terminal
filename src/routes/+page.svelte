d1489979dfb1a25396d02749a606ae383d1ee934
Pop <poplizardo4321@gmail.com>
2025-11-29 17:43:05 +0800

    Removed i and the redundant shell response if block

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,11 @@
 </script>
 <div id="bg-container" class="h-screen w-screen flex items-center justify-center p-8" style="background-color: var(--color-bg-primary)">
     <div id="term-interface" class="block w-full h-full border-2 rounded-xl box-border p-5 overflow-auto" style="border-color: var(--color-secondary)">
-        {#each termBlocks as block, i}
+        {#each termBlocks as block}
             {#if block.type === "prompt"}
                 <ShellPrompt bind:termBlocks={termBlocks} command={block.command} isDone={block.isDone}/>
             {:else if block.type === "response"}
-                <ShellResponse command={block.command}/>
-            {:else if block.type === "response"}
-                <ShellResponse command={block.command}/>
+                <ShellResponse command={block.command} />
             {/if}
         {/each}
     </div>
